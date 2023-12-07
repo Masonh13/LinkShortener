11c3b4b1011ae872aa3ac132f0743ed46c018eae
Masonh13 <masonxhoey@gmail.com>
2023-12-06 21:04:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 var privateKey  = fs.readFileSync('ssl/server.key', 'utf8');
 var certificate = fs.readFileSync('ssl/server.crt', 'utf8');
 
+const PW = "stalindidnothingwrong";
+
 var credentials = {key: privateKey, cert: certificate};
 app.use(express.json());
 
@@ -17,10 +19,14 @@ app.get("/", (req, res) =>
     return res.end(String(fs.readFileSync("html/index.html")));
 });
 
-app.post("/shorten", (req, res) =>
+app.get("/delete", (req, res) =>
+{
+    return res.end(String(fs.readFileSync("html/delete.html")));
+});
+
+app.post("/api/shorten", (req, res) =>
 {
     let body = req.body;
-    console.log(body);
     let short = JSON.parse(String(fs.readFileSync("short.json")));
     let items = short.items;
     items = items.filter(item => item.endpoint != body.endpoint);
@@ -31,7 +37,21 @@ app.post("/shorten", (req, res) =>
     return res.end("DID IT LOL IM SO GOOD W RIZZ KAI CENAT FORTNITE SKIBIDI TOILET W RIZZ LIT AMON US FORTNITE DEFUALT DANCE COCK DICK SUCK DICK SUCK DICK SUCK DICK SUCK");
 });
 
+app.post("/api/delete", (req, res) =>
+{
+    let body = req.body;
+    if (body.pw != PW)
+        return res.status(404).end("U FUCKED UP");
+    let short = JSON.parse(String(fs.readFileSync("short.json")));
+    let items = short.items;
+    items = items.filter(item => item.endpoint != body.endpoint);
+    short.items = items;
+    fs.writeFileSync("short.json", JSON.stringify(short, null, 4));
+    return res.end("DID IT LOL IM SO GOOD W RIZZ KAI CENAT FORTNITE SKIBIDI TOILET W RIZZ LIT AMON US FORTNITE DEFUALT DANCE COCK DICK SUCK DICK SUCK DICK SUCK DICK SUCK");
+});
+
 app.get("/resources/script.js", (req, res) => res.end(String(fs.readFileSync("js/script.js"))));
+app.get("/resources/delete.js", (req, res) => res.end(String(fs.readFileSync("js/delete.js"))));
 
 let hserver = http.createServer(app);
 let hsserver = https.createServer(credentials, app);
